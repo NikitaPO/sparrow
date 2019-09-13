3e7aad00002ba103dca44dd8385abfe5dedadb89
NikitaPO <gigzim345@gmai.com>
2019-09-13 22:43:21 +0300

    first commit

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'get_theme_styles' );
+function get_theme_styles() {
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/default.css' );
+  wp_enqueue_style( 'layout', get_template_directory_uri() . '/assets/css/layout.css' );
+  wp_enqueue_style( 'media-queries', get_template_directory_uri() . '/assets/css/media-queries.css' );
+  wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr.js');
+}
+
+add_action( 'wp_footer', 'get_theme_scripts' );
+function get_theme_scripts() {
+  wp_enqueue_script( 'jquery' );
+  wp_enqueue_script( 'init', get_template_directory_uri() . '/assets/js/init.js');
+  wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js');
+  wp_enqueue_script( 'doubletaptogo', get_template_directory_uri() . '/assets/js/doubletaptogo.js');
+}
+
+add_action( 'after_setup_theme', 'register_theme_menus' );
+function register_theme_menus() {
+  register_nav_menu( 'top_menu', 'Меню в шапке сайта' );
+  register_nav_menu( 'bottom_menu', 'Меню в футере сайта' );
+}
---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Sparrow</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+   <!-- Mobile Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+   <!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" >
+
+  <?php wp_head() ?>
+
+</head>
+
+<body>
+
+   <!-- Header
+   ================================================== -->
+   <header>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <div class="logo">
+               <a href="<?php echo get_home_url(); ?>"><img alt="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.png"></a>
+            </div>
+
+            <nav id="nav-wrap">
+
+               <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	            <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+              <?php wp_nav_menu([
+                'theme_location'  => 'top_menu',
+                'container'       => null,
+                'menu_class'      => 'nav',
+                'menu_id'         => 'nav',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu'
+              ]); ?>
+
+            </nav> <!-- end #nav-wrap -->
+
+         </div>
+
+      </div>
+
+   </header> <!-- Header End -->
